e93fc8002c65eb80e0dcec3e427c14d9947dd09f
Abhisek753 <properachievement@gmail.com>
2025-10-25 12:57:35 +0530

    Development code

---
+++ Projects-Code/Javascript/Dom/Class1/index.js
@@ -0,0 +1,51 @@
+
+
+//  function Print(a,b){
+//     console.log("Hello, World!", a+b);
+//  }
+
+//  Print(2,4)
+
+    // const heading = document.getElementsByClassName('heading');
+
+//   const heading=document.querySelectorAll('.heading');
+//   heading.forEach(function(heading){
+//  console.log(heading.textContent)
+//   });
+//   const button = document.querySelector('#btn');
+//  console.log(button.textContent);
+
+// const para = document.querySelector('#para');
+// console.log(para.textContent);
+
+// const mybutton=document.querySelector('.mybtn');
+// console.log(mybutton.textContent);
+
+
+// const element1 = document.querySelector('#myElement1');
+// const element2 = document.querySelector('#myElement2');
+
+// element1.innerHTML="<strong> Abhisek </strong>";
+// console.log(element1.innerHTML);
+// console.log(element2.innerHTML);
+
+// let element=document.querySelector('.hello');
+// element.setAttribute('class','hello2');
+// element.setAttribute('id','text');
+// element.classList.add('hello3');
+// element.style.color="white"
+// element.style.fontSize="40px"
+
+// let element=document.querySelector('.parent');
+// let secondelement=document.querySelector('.hello');
+//  let newitem=document.createElement('p');
+//  newitem.textContent="Hello, Abhisek!";
+//  element.appendChild(newitem);
+//  let buttonelement=document.createElement('button');
+//  buttonelement.textContent="click me";
+//  secondelement.appendChild(buttonelement);
+
+
+    // let anchortag=document.querySelector('#google');
+    // anchortag.setAttribute('href','https://www.google.com');
+    // anchortag.setAttribute('target','_blank');
---
+++ Projects-Code/Javascript/Dom/Class2/index.js
@@ -0,0 +1,27 @@
+
+// let buttonelement = document.querySelector('.clickbtn');
+// buttonelement.addEventListener('click', function() {
+//   let element=document.querySelector('.heading');
+//   element.textContent="Dom Manipulation using JavaScript";
+// });
+
+
+let parentElement=document.querySelector(".parent")
+
+// creating button element
+let mybutton=document.createElement("button");
+mybutton.innerText="click me";
+
+// creating paragraph element
+let para=document.createElement("p");
+para.innerText="this is a paragraph";
+
+parentElement.appendChild(mybutton);
+parentElement.appendChild(para);
+
+mybutton.addEventListener("click",function(){
+  mybutton.innerText="you clicked me";
+  para.innerText="paragraph changed after clicking the button";
+});
+
+
