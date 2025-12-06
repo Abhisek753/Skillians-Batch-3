9a5446a48d1a2659fbaf10986067a968bb1845a8
Abhisek753 <properachievement@gmail.com>
2025-12-06 13:19:02 +0530

    navbar created

---
+++ Projects-Code/Frontend_Project/foodie/src/components/common/ProductCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductCard = ({product}) => {
+  return (
+    <div className='text-red-600'>{product.name}</div>
+  )
+}
+
+export default ProductCard
---
+++ Projects-Code/Frontend_Project/foodie/src/routes/AppRouter.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import HomePage from '../pages/HomePage'
+import CartPage from '../pages/CartPage'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from '../components/layout/Navbar'
+
+const AppRouter = () => {
+  return (
+    <div className='min-h-screen flex flex-col bg-gray-50'>
+      
+        <Navbar/>
+        <main>
+            <Routes>
+                <Route path='/' element={<HomePage/>}/>
+                <Route path='/cart' element={<CartPage/>}/>
+                
+            </Routes>
+        </main>
+        
+    </div>
+  )
+}
+
+export default AppRouter
